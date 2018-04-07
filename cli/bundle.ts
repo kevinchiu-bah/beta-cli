efab5d8550c630b8bf198b1eae6b1e3aa0600247
Kevin Chiu <chiu_kevin@bah.com>
2018-04-07 19:23:32 -0400

    - Added ability to glob via `path` input to `inquirer` - Added catching ability for multiple directory result from glob (taking first item in list)

--- cli/bundle.ts
+++ cli/bundle.ts
@@ -3,10 +3,10 @@ import * as chalk from 'chalk';
 import * as fs from 'fs';
 import * as glob from 'glob';
 import * as inquirer from 'inquirer';
-import { map } from 'lodash';
+import { isNil, map, pick } from 'lodash';
 import * as path from 'path';
 import * as process from 'process';
-import { env } from 'shelljs';
+import { env, exec, ls } from 'shelljs';
 // import { encode } from './encode';
 
 const mv = (source, target) => {
@@ -14,57 +14,174 @@ const mv = (source, target) => {
   console.log(`${chalk.green('[Renaming]')} ${chalk.gray(source)} => ${chalk.cyan(target)}`);
 };
 
-const bundle = (dirname, prefix, locale) => {
-  const cwd = path.resolve(process.cwd(), dirname);
-  const options = {
-    cwd: cwd,
-    sync: true,
-  };
-
-  let files;
-  let basePath;
-  let target;
-  let ext;
-  let locale;
-  let dir$;
-
-  files = glob.sync('*.{jpg,jpeg.png}', options);
-  files.forEach(source => {
-    basePath = path.dirname(path.join(cwd, source));
-    source = `${basePath}/${source}`;
-    ext = path.extname(source);
-    target = `${basePath}/cover${ext}`;
-    mv(source, target);
-  });
-
-  files = glob.sync('*.{srt,ass,ssa,sub,vob}', options);
-  files.forEach(source => {
-    basePath = path.dirname(path.join(cwd, source));
-    source = `${basePath}/${source}`;
-    ext = path.extname(source);
-    target = `${basePath}/${prefix}.${locale}${ext}`;
-    mv(source, target);
-    // encode(target);
-    // fs.unlinkSync(source);
-  });
-
-  files = glob.sync('*.{mp4,avi,mkv}', options);
-  files.forEach(source => {
-    basePath = path.dirname(path.join(cwd, source));
-    source = `${basePath}/${source}`;
-    ext = path.extname(source);
-    target = `${basePath}/${prefix}${ext}`;
-    mv(source, target);
-  });
-
-  dir$ = cwd.split('/');
-  dir$.pop();
-  dir$.push(prefix)
-  dir$ = dir$.join('/');
-
-  mv(cwd, dir$);
+interface Options {
+  prefix?: string;
+  source?: string;
+  locale?: string;
 };
 
+interface DependencyOptions {
+  glob: G.IOptions,
+};
+
+class Bundle {
+  private _source;
+  private _prefix;
+  private _locale;
+
+  private params: DependencyOptions = {
+    glob: {
+      sync: true,
+      cwd: process.cwd(),
+    },
+  };
+
+  constructor(source?, prefix = 'The.Bundle', locale = 'en') {
+    this.options = {
+      source,
+      prefix,
+      locale,
+    };
+  }
+
+  /**
+   * [Getters/Setters] @source
+   */
+  get source(): string {
+    return this._source;
+  }
+
+  set source(source: string) {
+    if(!isNil(source)) {
+      this._source = source;
+      this.params.glob.cwd = source;
+    }
+  }
+
+  /**
+   * [Getters/Setters] @prefix
+   */
+  get prefix(): string {
+    return this._prefix;
+  }
+
+  set prefix(prefix = 'en') {
+    if(!isNil(prefix)) {
+      this._prefix = prefix;
+    }
+  }
+
+  /**
+   * [Getters/Setters] @locale
+   */
+  get locale(): string {
+    return this._locale;
+  }
+
+  set locale(locale: string = null) {
+    if(!isNil(locale)) {
+      this._locale = locale;
+    }
+  }
+
+  get options(): Options {
+    return {
+      source: this._source,
+      prefix: this._prefix,
+      locale: this._locale,
+    };
+  }
+
+  /**
+   * [Getters/Setters] @options
+   */
+  set options(options: Options = {}) {
+    const keys = [
+      'source',
+      'prefix',
+      'locale',
+    ];
+
+    map(pick(options, keys), (value, key) => {
+      this[key] = value;
+    });
+  }
+
+  _single() {
+    const params = this.params;
+    const cwd = this.source;
+    const prefix = this.prefix;
+    const locale = this.locale;
+
+    let files;
+    let basePath;
+    let target;
+    let ext;
+    let dir$;
+
+    files = glob.sync('*.{jpg,jpeg.png}', params.glob);
+    files.forEach(source => {
+      basePath = path.dirname(path.join(cwd, source));
+      source = `${basePath}/${source}`;
+      ext = path.extname(source);
+      target = `${basePath}/cover${ext}`;
+      mv(source, target);
+    });
+
+    files = glob.sync('*.{srt,ass,ssa,sub,vob}', params.glob);
+    files.forEach(source => {
+      basePath = path.dirname(path.join(cwd, source));
+      source = `${basePath}/${source}`;
+      ext = path.extname(source);
+      target = `${basePath}/${prefix}.${locale}${ext}`;
+      mv(source, target);
+      // encode(target);
+      // fs.unlinkSync(source);
+    });
+
+    files = glob.sync('*.{mp4,avi,mkv}', params.glob);
+    files.forEach(source => {
+      basePath = path.dirname(path.join(cwd, source));
+      source = `${basePath}/${source}`;
+      ext = path.extname(source);
+      target = `${basePath}/${prefix}${ext}`;
+      mv(source, target);
+    });
+
+    dir$ = cwd.split('/');
+    dir$.pop();
+    dir$.push(prefix)
+    dir$ = dir$.join('/');
+
+    mv(cwd, dir$);
+  }
+
+  _multi() {
+    console.log(`${chalk.yellow('[Warning]')} This method has not been implemented yet! Exiting.`);
+    process.exit();
+  }
+
+
+  run() {
+    glob('*.{mp4,avi,mkv}', (error, files) => {
+      switch(files.length) {
+        case 0:
+        case 1:
+          console.log(`${chalk.cyan('\n[Bundle Type]')} Single`);
+          this._single();
+          break;
+
+        default:
+          console.log(`${chalk.cyan('\n[Bundle Type]')} Multi`);
+          this._multi();
+      }
+    });
+  }
+};
+
+/**
+ * Initializer/Bootstrap
+ */
 const init = () => {
   inquirer.prompt([
     {
@@ -82,22 +199,48 @@ const init = () => {
       message: 'Enter the language suffix or your srt',
       default: 'en',
     }
-  ]).then(answers => {
-    const vars = answers.path.match(/\$[a-z0-9]+/i);
+  ]).then(params => {
+    const bundle: Bundle;
+    const vars = params.path.match(/\$[a-z0-9]+/ig);
+    const options: ExecOptions = {
+      silent: true,
+    };
 
     if(vars) {
       vars.forEach(key => {
-        const key$ = key.replace(/^\$/, '');
+        const key$ = key.replace(/^\$/g, '');
         const value$ = env[key$];
 
         if(value$) {
-          answers.path = answers.path.replace(key, value$);
+          params.path = params.path.replace(key, value$);
         }
       });
-
-      bundle(answers.path, answers.prefix, answers.locale);
     }
-   }).catch((e) => { console.log(e); });
+
+    // Santize the user's path input
+    // 1. Remove extra slashes
+    // 2. Remove trailing slashes
+    params.path = params.path
+      .replace(/\/\/+/g, '/')
+      .replace(/\/$/g, '');
+
+    const paths = glob.sync(`${params.path}/`, { absolute: true });
+
+    if(paths.length) {
+      params.path = paths[0];
+      bundle = new Bundle(params.path, params.prefix, params.locale);
+      bundle.run();
+    } else {
+      console.log([
+        chalk.red('[Error]'),
+        `${params.path} is not or does not contain any valid directories!`,
+        chalk.yellow('Aborting'),
+      ].join(' '));
+      process.exit();
+    }
+   }).catch((e) => {
+     console.log(e);
+   });
 };
 
 export default init;
